0d7700fad2d40aad4d409c385e5abbed709c992f
Sky <anvandp18@gmail.com>
2018-09-12 16:32:34 -0700

    step 4

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import FilmDetails from './FilmDetails';
 import FilmListing from './FilmListing';
+import TMDB from './TMDB';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="film-library">
-          <FilmDetails />
-          <FilmListing />
+          <FilmListing films = {TMDB.films}/>
+          <FilmDetails films = {TMDB.films}/>
         </div>
       </div>
     );
