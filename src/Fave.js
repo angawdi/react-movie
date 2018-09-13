e53b43eb8b88d61e55466fb71699334d262f25fa
Sky <anvandp18@gmail.com>
2018-09-12 17:40:19 -0700

    step 3

--- src/Fave.js
+++ src/Fave.js
@@ -3,10 +3,16 @@ import React, { Component } from 'react';
 export default class Fave extends Component {
 	constructor(props) {
 		super();
+		this.state = {
+			isFave = false;
+		}
 	}
 
 	handleClick = (e) => {
 		e.stopPropagation();
+		this.setState({
+			isFave: !this.state.isFave;
+		})
 		console.log('Handling fave click');
 	}
 	render() {
