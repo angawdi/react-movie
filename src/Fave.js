6996f883a05f2ed01934a670fe901b57897a9dcc
Sky <anvandp18@gmail.com>
2018-09-12 17:19:50 -0700

    task 2 step 2

--- src/Fave.js
+++ src/Fave.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
 export default class Fave extends Component {
+	handleClick = (e) => {
+		console.log('Handling fave click');
+	}
 	render() {
 		return (
-			<div className="film-row-fave add_to_queue">
+			<div className="film-row-fave add_to_queue" conClick={this.handleClick}>
   			<p className="material-icons">add_to_queue</p>
 </div>
 		);
--- src/FilmListing.js
+++ src/FilmListing.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import FilmRow from './FilmRow';
 
 export default class FilmListing extends Component {
+	handleFilterClick = (filter) => {
+		console.log('setting filter to ', filter);
+	} 
 	render() {
 		const allFilms = this.props.films.map( (film) => (<FilmRow film={film}/>));
 		return (
